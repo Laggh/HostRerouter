979c5734aac3727467a7a3604cd5cab2acb52c8d
Renan Andrade <77841497+Laggh@users.noreply.github.com>
2026-01-25 23:38:01 -0300

    dando uma melhorada

--- main.go
+++ main.go
@@ -1,19 +1,51 @@
 package main
 
 import (
+	"fmt"
+	"image/color"
+
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("Hello World")
+	w.SetTitle("Host Rerouter")
 
-	button := widget.NewButton("Click Me", func() {
-		w.SetContent(widget.NewLabel("Button Clicked!"))
+	tamanho := 6
+	table := widget.NewTable(
+		//lenght
+		func() (int, int) {
+			return tamanho, 3
+		},
+		//create
+		func() fyne.CanvasObject {
+			c := container.NewStack()
+			r := canvas.NewRectangle(color.Transparent)
+			r.SetMinSize(fyne.NewSize(40, 40))
+			c.Add(r)
+			return c
+		},
+		func(i widget.TableCellID, o fyne.CanvasObject) {
+			row, col := i.Row, i.Col
+			o.(*fyne.Container).Objects = nil
+			o.(*fyne.Container).Add(widget.NewLabel(fmt.Sprintf("R %d C %d", row, col)))
+			o.(*fyne.Container).Add(widget.NewLabel(fmt.Sprintf("R %d C %d", row, col)))
+		},
+	)
+
+	btn := widget.NewButton("Action", func() {
+		fmt.Println("Printando algo no console!")
+		tamanho++
 	})
+	split := container.NewHSplit(table, btn)
+	split.SetOffset(0.7)
 
-	// Set initial content
-	w.SetContent(button)
+	w.Resize(fyne.NewSize(600, 400))
+	w.SetContent(split)
 	w.ShowAndRun()
 }
